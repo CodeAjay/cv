b1ab2e7565b7b0b66ea768e1c0bce416b40c61a2
CodeAjay <58427577+CodeAjay@users.noreply.github.com>
2020-08-22 20:34:25 +0530

    Add files via upload

---
+++ javascript/LoveCalculator/index.js
@@ -0,0 +1,39 @@
+function love(){
+    var first = document.getElementById("first").value;
+    var second = document.getElementById("second").value;
+    if(first == ""){
+        alert('Please enter your name');
+       }else if (first.length <=2 ) {
+        alert('Min lenght is 3')
+       }else if(!isNaN(first)){
+        alert('Numbers are not allowed');
+       }
+     
+       else if(second == ""){
+        alert('Please enter your love name');
+       }else if (second.length <=2 ) {
+        alert('Min lenght is 3')
+       }else if(!isNaN(second)){
+        alert('Numbers are not allowed');
+       }
+       else{    
+        var loveCal = Math.floor(Math.random()*100)+1;
+        document.querySelector("img").setAttribute("src", "https://source.unsplash.com/200x200/?love");
+        document.querySelector("form").classList.add("smooth");
+        if(loveCal>90){
+        document.querySelector("h1").innerHTML= "Your Love score is: "+loveCal+"%";
+        document.querySelector("h2").innerHTML= first +" " +second+ " are a great couples";
+        }
+        else if
+        (loveCal>60 && loveCal<90){
+        document.querySelector("h1").innerHTML= "Your Love score is: "+loveCal+"%";
+        document.querySelector("h2").innerHTML= first +" " +second+ " are a vey good couples";
+        } 
+        else{
+        document.querySelector("h1").innerHTML= "Your Love score is: "+loveCal+"%";
+        document.querySelector("h2").innerHTML= first +" " +second+ " are a good couples";
+        } 
+    }
+   
+
+}
