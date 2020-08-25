b97bb71c54d480a4f3ae7f8c0cce22fc4229a626
CodeAjay <58427577+CodeAjay@users.noreply.github.com>
2020-08-25 18:51:17 +0530

    Update script.js

--- javascript/clock/script.js
+++ javascript/clock/script.js
@@ -1,5 +1,7 @@
 setInterval (() => {
   d= new Date();
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   h= d.getHours();
   m= d.getMinutes();
   s=d.getSeconds();
@@ -10,4 +12,13 @@ setInterval (() => {
   hour.style.transform = `rotate(${hrotation}deg)`;
   min.style.transform= `rotate(${mrotation}deg)`;
   sec.style.transform= `rotate(${srotation}deg)`;
+
+  document.querySelector(".date").innerHTML=days[d.getDay()] + "<br>" + d.getDate() +  "-" + months[d.getMonth()]+"-" +d.getFullYear();
+  if(h>12){
+    h=24-h;
+  document.querySelector(".time").innerHTML=h+":"+d.getMinutes()+":"+d.getSeconds()+" PM";
+  }
+  else{
+    document.querySelector(".time").innerHTML=h+":"+d.getMinutes()+":"+d.getSeconds()+"AM";
+  }
 }, 1000);
