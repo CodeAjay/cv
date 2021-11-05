32004bc91bcd1afbfc2c2eedc69dff9098899983
CodeAjay <58427577+CodeAjay@users.noreply.github.com>
2021-11-05 21:42:36 +0530

    Update script.js

--- javascript/clock/script.js
+++ javascript/clock/script.js
@@ -2,9 +2,9 @@ setInterval (() => {
   d= new Date();
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var h= d.getHours();
-  var m= d.getMinutes();
-  var s=d.getSeconds();
+  h= d.getHours();
+  m= d.getMinutes();
+  s=d.getSeconds();
   hrotation=30*h+m/2;
   mrotation= 6*m;
   srotation= 6*s;
@@ -14,11 +14,11 @@ setInterval (() => {
   sec.style.transform= `rotate(${srotation}deg)`;
 
   document.querySelector(".date").innerHTML=days[d.getDay()] + "<br>" + d.getDate() +  "-" + months[d.getMonth()]+"-" +d.getFullYear();
-  if(h>12){
-    var nh=24-h;
-  document.querySelector(".time").innerHTML=nh+":"+d.getMinutes()+":"+d.getSeconds()+" PM";
+  if(d.getHours()>12){
+
+  document.querySelector(".time").innerHTML=24-d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+" PM";
   }
   else{
-    document.querySelector(".time").innerHTML=h+":"+d.getMinutes()+":"+d.getSeconds()+"AM";
+    document.querySelector(".time").innerHTML=d.getHours()+":"+d.getMinutes()+":"+d.getSeconds()+"AM";
   }
 }, 1000);
